099fe2a52eb6b11c3609137ea97067fdc3a364d2
mroghani <m.roghani1212@gmail.com>
2021-06-18 22:41:26 +0430

    mutables.

--- helpers/helpers.cc
+++ helpers/helpers.cc
@@ -15,7 +15,7 @@ Node helpers::make_constant(const Constant& c) {
 
     // LI v0 {CONSTANT}
     std::ostringstream text;
-    text << "LI $v0 " << c.value;
+    text << "LI $v0, " << c.value << std::endl;
     
 
     node.code.text = text.str();
@@ -31,12 +31,25 @@ Node helpers::make_mutable(driver & drv, std::string id, yy::location & loc, Nod
 
     auto var = drv.get_variable(id, loc);
 
+
     // code gen:
     // find the address of mutable and put it in v1
     // if it's an array the index should be found in v0
-    // v1 = sp + offset + (v0 * 4)
+    // v1 = sp + (offset * 4) + (v0 * 4)
 
-    
+    std::ostringstream text;
+    if (ind != nullptr) {
+        text << ind->code.text;
+    }
+    text << "ADDI $v1, $sp, " << var.offset * 4 << std::endl; // v1 = sp + offset * 4
+    if (ind != nullptr) { // v1 = v1 + v0 * 4
+        text << "SLL $v0, $v0, 2" << std::endl; // v0 = v0 * 4
+        text << "ADD $v1, $v1, $v0" << std::endl; // v1 = v1 * v0
+    }
+
+    node.code.text = text.str();
+
+
 
     return node;
 }
